5647ac0aa92796f551db27780b2c1a07d245cb5f
anddani <andredanielsson93@gmail.com>
2026-01-10 11:12:28 -0800

    Remove empty line

--- src/msg/util.rs
+++ src/msg/util.rs
@@ -4,7 +4,6 @@ use crate::model::{Line, SectionType};
 
 /// Count visible lines between two raw line indices (exclusive of end).
 /// Used to calculate scroll offsets in terms of visible lines.
-
 pub fn visible_lines_between(
     lines: &[Line],
     start: usize,
