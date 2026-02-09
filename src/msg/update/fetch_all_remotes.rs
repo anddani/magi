3fcd1932bb307550eaffd47412ae48b0441e52d2
anddani <andredanielsson93@gmail.com>
2026-02-09 11:52:53 -0800

    Fix formating

--- src/msg/update/fetch_all_remotes.rs
+++ src/msg/update/fetch_all_remotes.rs
@@ -3,11 +3,7 @@ use crate::{model::Model, msg::Message};
 use super::pty_helper::execute_pty_command;
 
 pub fn update(model: &mut Model) -> Option<Message> {
-    let args = vec![
-        "fetch".to_string(),
-        "-v".to_string(),
-        "--all".to_string(),
-    ];
+    let args = vec!["fetch".to_string(), "-v".to_string(), "--all".to_string()];
 
     execute_pty_command(model, args, "Fetch all".to_string())
 }
