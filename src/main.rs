e75e7bab90aa939dbbbf91c49a255cad4ae60aba
anddani <andredanielsson93@gmail.com>
2026-02-17 08:35:38 -0800

    Fix clippy

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,36 @@ fn main() -> MagiResult<()> {
 
 fn parse_args() -> Option<PathBuf> {
     let args: Vec<String> = env::args().skip(1).collect();
-    let mut args_iter = args.iter().peekable();
+    let mut args_iter = args.iter();
 
-    while let Some(arg) = args_iter.next() {
-        match arg.as_str() {
-            "-h" | "--help" => {
-                print_help();
-                process::exit(0);
-            }
-            "-V" | "--version" => {
-                println!("magi {}", env!("CARGO_PKG_VERSION"));
-                process::exit(0);
-            }
-            "-C" => {
-                let path = args_iter.next().unwrap_or_else(|| {
-                    eprintln!("error: option '-C' requires an argument");
-                    process::exit(1);
-                });
-                return Some(PathBuf::from(path));
-            }
-            arg if arg.starts_with("-C") => {
-                // Handle -C<path> (no space)
-                return Some(PathBuf::from(&arg[2..]));
-            }
-            arg => {
-                eprintln!("error: unknown option '{}'", arg);
-                print_help();
+    let arg = args_iter.next()?;
+
+    match arg.as_str() {
+        "-h" | "--help" => {
+            print_help();
+            process::exit(0);
+        }
+        "-V" | "--version" => {
+            println!("magi {}", env!("CARGO_PKG_VERSION"));
+            process::exit(0);
+        }
+        "-C" => {
+            let path = args_iter.next().unwrap_or_else(|| {
+                eprintln!("error: option '-C' requires an argument");
                 process::exit(1);
-            }
+            });
+            Some(PathBuf::from(path))
+        }
+        arg if arg.starts_with("-C") => {
+            // Handle -C<path> (no space)
+            Some(PathBuf::from(&arg[2..]))
+        }
+        arg => {
+            eprintln!("error: unknown option '{}'", arg);
+            print_help();
+            process::exit(1);
         }
     }
-
-    None
 }
 
 fn print_help() {
