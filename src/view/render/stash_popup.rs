63742c2d3712363f8c034b0b467c234be124568f
Andre Danielsson <andredanielsson93@gmail.com>
2026-02-25 09:35:51 -0800

    Implement include untracked argument

--- src/view/render/stash_popup.rs
+++ src/view/render/stash_popup.rs
@@ -1,16 +1,51 @@
+use std::collections::HashSet;
+
 use ratatui::{
     style::{Modifier, Style},
     text::{Line, Span},
 };
 
 use super::popup_content::{CommandPopupContent, PopupColumn, PopupRow};
-use crate::config::Theme;
+use crate::{
+    config::Theme,
+    model::{
+        Model,
+        arguments::{Arguments::StashArguments, StashArgument},
+    },
+    view::render::util::argument_line,
+};
 
-pub fn content(theme: &Theme) -> CommandPopupContent<'static> {
+pub fn content(theme: &Theme, model: &Model) -> CommandPopupContent<'static> {
     let key_style = Style::default()
         .fg(theme.local_branch)
         .add_modifier(Modifier::BOLD);
     let desc_style = Style::default();
+
+    let selected_args: HashSet<StashArgument> =
+        if let Some(StashArguments(ref args)) = model.arguments {
+            args.clone()
+        } else {
+            HashSet::new()
+        };
+
+    let arguments: Vec<Line> = StashArgument::all()
+        .iter()
+        .map(|arg| {
+            argument_line(
+                theme,
+                arg.key(),
+                arg.description(),
+                arg.flag(),
+                model.arg_mode,
+                selected_args.contains(arg),
+            )
+        })
+        .collect();
+
+    let arguments_col = PopupColumn {
+        title: Some("Arguments"),
+        content: arguments,
+    };
 
     let stash = PopupColumn {
         title: Some("Stash"),
@@ -22,8 +57,13 @@ pub fn content(theme: &Theme) -> CommandPopupContent<'static> {
 
     CommandPopupContent {
         title: "Stash",
-        rows: vec![PopupRow {
-            columns: vec![stash],
-        }],
+        rows: vec![
+            PopupRow {
+                columns: vec![arguments_col],
+            },
+            PopupRow {
+                columns: vec![stash],
+            },
+        ],
     }
 }
