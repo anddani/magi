25067ca78a4e238463d6badaab114f127fa3e607
Andre Danielsson <andredanielsson93@gmail.com>
2026-02-25 12:29:47 -0800

    Fmt

--- src/msg/update/show_stash_apply_select.rs
+++ src/msg/update/show_stash_apply_select.rs
@@ -11,9 +11,10 @@ pub fn update(model: &mut Model) -> Option<Message> {
     // If cursor is on a stash line, apply it immediately
     let cursor_pos = model.ui_model.cursor_position;
     if let Some(line) = model.ui_model.lines.get(cursor_pos)
-        && let LineContent::Stash(entry) = &line.content {
-            return Some(Message::StashApply(format!("stash@{{{}}}", entry.index)));
-        }
+        && let LineContent::Stash(entry) = &line.content
+    {
+        return Some(Message::StashApply(format!("stash@{{{}}}", entry.index)));
+    }
 
     // Otherwise collect all stash entries and show a selection popup
     let stashes: Vec<String> = model
