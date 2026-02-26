14efeb9f2c6a728dacc410f736798f38c73588b0
anddani <andredanielsson93@gmail.com>
2026-02-25 21:40:15 -0800

    Fix broken nix flake

--- flake.nix
+++ flake.nix
@@ -11,53 +11,72 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , naersk
-    , rust-overlay
-    , ...
-    } @ inputs:
+    {
+      self,
+      nixpkgs,
+      naersk,
+      rust-overlay,
+      ...
+    }@inputs:
     let
       inherit (nixpkgs) lib;
-      systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       eachSystem =
         with lib;
         f: foldAttrs mergeAttrs { } (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems);
     in
-    eachSystem
-      (
-        system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          toolchain = pkgs.rust-bin.stable."1.92.0".default.override {
-            targets = [
-              "x86_64-apple-darwin"
-              "aarch64-apple-darwin"
-              "x86_64-unknown-linux-gnu"
-              "aarch64-unknown-linux-gnu"
-            ];
-          };
-          naersk' = pkgs.callPackage naersk {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-        in
-        {
-          packages.default = naersk'.buildPackage { src = ./.; };
-          packages.clippy = naersk'.buildPackage {
-            src = ./.;
-            mode = "clippy";
-          };
-          checks.default = naersk'.buildPackage {
-            src = ./.;
-            mode = "test";
-          };
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [ pkgs.perl toolchain pkgs.rust-analyzer ];
-          };
-        }
-      );
+    eachSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable."1.92.0".default.override {
+          targets = [
+            "x86_64-apple-darwin"
+            "aarch64-apple-darwin"
+            "x86_64-unknown-linux-gnu"
+            "aarch64-unknown-linux-gnu"
+          ];
+        };
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        magi = naersk'.buildPackage {
+          src = ./.;
+          propagatedBuildInputs = with pkgs; [
+            openssl
+            pkg-config
+          ];
+        };
+      in
+      {
+        packages.default = magi;
+        packages.magi = magi;
+        packages.clippy = naersk'.buildPackage {
+          src = ./.;
+          mode = "clippy";
+        };
+        checks.default = naersk'.buildPackage {
+          src = ./.;
+          mode = "test";
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.openssl
+            pkgs.pkg-config
+            pkgs.perl
+            toolchain
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
 }
