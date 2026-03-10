8555de7f051492cd0b05e12ee517beefeacea19f
André Danielsson <andredanielsson93@gmail.com>
2026-03-10 08:57:28 -0700

    Exit visual mode when exiting log view mode (#397)

--- src/msg/update/show_log.rs
+++ src/msg/update/show_log.rs
@@ -20,6 +20,8 @@ pub fn update(model: &mut Model, log_type: LogType) -> Option<Message> {
             model.ui_model.lines = lines;
             model.ui_model.cursor_position = 0;
             model.ui_model.scroll_offset = 0;
+            // Exit visual mode so it doesn't get carried over from Status view
+            model.ui_model.visual_mode_anchor = None;
 
             // Switch to log view mode
             model.view_mode = ViewMode::Log(log_type, false);
