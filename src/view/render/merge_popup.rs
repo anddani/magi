1d823bfe3446f7b577d90242c1b3dd3dbfe70ef9
anddani <andredanielsson93@gmail.com>
2026-07-19 13:45:07 -0700

    Implement merge squash
    Closes #367

--- src/view/render/merge_popup.rs
+++ src/view/render/merge_popup.rs
@@ -38,6 +38,7 @@ pub fn content<'a>(
             command_description(theme, model.arg_mode, "n", t.cmd_merge_no_commit),
             command_description(theme, model.arg_mode, "a", t.cmd_merge_absorb),
             command_description(theme, model.arg_mode, "p", t.cmd_merge_preview),
+            command_description(theme, model.arg_mode, "s", t.cmd_merge_squash),
         ],
     };
 
